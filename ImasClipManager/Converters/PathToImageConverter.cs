e3f80a5df164af094365a40fbcc288603ca06147
Ang107 <kosukenabeta1007@gmail.com>
2025-11-28 22:59:25 +0900

    サムネの保持ロジックの改善

---
+++ ImasClipManager/Converters/PathToImageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using ImasClipManager.Helpers;
+
+namespace ImasClipManager.Converters
+{
+    public class PathToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string path)
+            {
+                return ImageHelper.LoadBitmapNoLock(path);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ImasClipManager/Helpers/ImageHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace ImasClipManager.Helpers
+{
+    public static class ImageHelper
+    {
+        public static BitmapImage LoadBitmapNoLock(string path)
+        {
+            if (string.IsNullOrEmpty(path) || !File.Exists(path)) return null;
+
+            try
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad; // ★重要: メモリに展開してファイルを解放
+                bitmap.UriSource = new Uri(path);
+                bitmap.EndInit();
+                bitmap.Freeze(); // スレッド間での共有を許可
+                return bitmap;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
