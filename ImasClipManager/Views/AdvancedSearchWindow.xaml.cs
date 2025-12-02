310a1e558e458f6bf06fc83909084479b8a6466a
Ang107 <kosukenabeta1007@gmail.com>
2025-12-03 01:18:45 +0900

    詳細検索の実装(バグあり)

--- ImasClipManager/Views/AdvancedSearchWindow.xaml.cs
+++ ImasClipManager/Views/AdvancedSearchWindow.xaml.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System.Windows;
+
+namespace ImasClipManager.Views
+{
+    public partial class AdvancedSearchWindow : Window
+    {
+        public AdvancedSearchWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
